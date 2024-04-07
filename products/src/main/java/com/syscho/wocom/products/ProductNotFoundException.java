51421739f1877b48bf7c16dab5ea03adee501bfd
praveen <praveen369soni@gmail.com>
2024-04-07 19:53:27 +0530

    Global exception handler

--- products/src/main/java/com/syscho/wocom/products/ProductNotFoundException.java
+++ products/src/main/java/com/syscho/wocom/products/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package com.syscho.wocom.products;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
 
     public ProductNotFoundException(String message) {
